c6342aefcef1fc6b4601531900c0db61485a4c9d
brunoleomont <brunoleomont@gmail.com>
2021-04-13 11:42:05 -0300

    Update EntrevistaEmbaracosa.java

--- Inter-Java-Developer/EntrevistaEmbaracosa.java
+++ Inter-Java-Developer/EntrevistaEmbaracosa.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
-
 
 public class EntrevistaEmbaracosa {
   public static void main(String[] args) throws IOException {
